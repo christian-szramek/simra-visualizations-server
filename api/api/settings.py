61c404a0f45437cefdee303d046921ac8b9f13c1
Christian Szramek <christian.szramek@web.de>
2025-12-18 10:52:50 +0100

    Update ALLOWED_HOSTS to allow all hosts

--- api/api/settings.py
+++ api/api/settings.py
@@ -26,7 +26,8 @@ SECRET_KEY = "zs!2%fi$!d$2yub#a98pwi5&b=a=+70^q)3brlnxyb+u94ql4h"
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["127.0.0.1"]  # 207.180.205.80
+# ALLOWED_HOSTS = ["127.0.0.1"]  # 207.180.205.80
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
